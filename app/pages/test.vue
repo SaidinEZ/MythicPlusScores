e02442474288ac47abe26514ceac86eeead4942f
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2021-12-05 15:14:52 +0000

    Update test.vue

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,1 +1,15 @@
+<template>
+  <div>
+    <h1>{{ earth.title }}</h1>
+    <p>{{ earth.description }}</p>
+  </div>
+</template>
 
+<script>
+export default {
+  async asyncData({ $axios }) {
+    const earth = await $axios.$get(`https://api.nuxtjs.dev/planets/earth`)
+    return { earth }
+  },
+}
+</script>
