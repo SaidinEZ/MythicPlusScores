a0c920ab715d9236f7b125529be78dbb218e72b7
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2021-12-05 20:16:50 +0000

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -19,9 +19,6 @@
                   
                 </th>
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Profile
                 </th>
                 <th scope="col" class="relative px-6 py-3">
