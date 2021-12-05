17bde9bd470cdc59e020ca756358a8a0a374e5cb
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2021-12-05 16:02:01 +0000

    Update test.vue

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,10 +1,9 @@
-<template>
-  <div>
-    <h1>{{ earth.title }}</h1>
-    <p>{{ earth.description }}</p>
-  </div>
-  
+<template> 
   <div class="flex flex-col">
+    <div>
+      <h1>{{ earth.title }}</h1>
+      <p>{{ earth.description }}</p>
+    </div>
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
