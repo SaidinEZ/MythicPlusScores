3a42cda4373f489c8e8014c1279d41e32305214c
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2021-12-05 19:46:55 +0000

    Update test.vue

--- app/pages/test.vue
+++ app/pages/test.vue
@@ -1,8 +1,5 @@
 <template> 
   <div class="flex flex-col">
-    <div>
-      <h1>Test</h1>
-    </div>
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
         <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
@@ -14,9 +11,6 @@
                 </th>
                 <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Score
-                </th>
-                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                  Status
                 </th>
                 <th scope="col" class="col-span-2 px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                   Dungeons
@@ -51,13 +45,16 @@
                   <div class="text-sm text-gray-500">1427</div>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="inline-flex text-sm text-gray-900">HOA</div>
                   <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                    Active
+                    16
                   </span>
                 </td>
-                <td class="px-6 py-4 whitespace-nowrap">
-                  <div class="text-sm text-gray-900">HOA</div>
-                  <div class="text-sm text-gray-500">+16</div>
+                 <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="inline-flex text-sm text-gray-900">DOS</div>
+                  <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                    16
+                  </span>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                   <a :href="character.profile_url" class="text-indigo-600 hover:text-indigo-900">Profile</a>
