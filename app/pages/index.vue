3c7227d6e942ee0a2164a00a35bd304234247569
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2021-12-06 16:15:56 +0000

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -452,7 +452,7 @@
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap">
                   <div class="text-sm text-gray-900">Total</div>
-                  <div class="text-sm" :style="color: pogfel.mythic_plus_scores_by_season[0].segments.all.color">{{ enkazin.mythic_plus_scores_by_season[0].segments.all.score }}</div>
+                  <div class="text-sm" :style="color: enkazin.mythic_plus_scores_by_season[0].segments.all.color">{{ enkazin.mythic_plus_scores_by_season[0].segments.all.score }}</div>
                 </td>
                 <td class="px-6 py-4 whitespace-nowrap">
                   <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800 text-center">
