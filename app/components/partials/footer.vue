1f94b9a70d10be35d9f447f78436e2d0398858a6
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2021-12-06 22:13:46 +0000

    Update footer.vue

--- app/components/partials/footer.vue
+++ app/components/partials/footer.vue
@@ -1,10 +1,8 @@
 <template>
   <footer class="footer -mx-4 md:mx-0">
     <div class="container mx-auto pb-6 flex justify-center text-white">
-      <div class="header__logo">
-        <nuxt-link to="/">
+      <div>
           <img :src="logo" alt="Logo" class="fill-current" />
-        </nuxt-link>
       </div>
       <p>
         Enkazin
