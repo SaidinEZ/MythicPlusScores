77757d92e0bde3e8527e24f54b17d63285941117
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2021-12-07 02:13:05 +0000

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,8 @@
-<template> 
+<template>
+  <div>
+    <h1>{{ androl.mythic_plus_best_runs.find(x => x.short_name === 'HOA').mythic_level }}</h1>
+  </div>
+
   <div class="flex flex-col mb-2">
     <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
       <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
