f95e30ca6e3525fc806f27eafdc8844599b7bb3b
SaidinEZ <95552792+SaidinEZ@users.noreply.github.com>
2021-12-20 13:15:26 +0000

    Update header.vue

--- app/components/partials/header.vue
+++ app/components/partials/header.vue
@@ -9,7 +9,7 @@
     <nav class="nav ml-auto">
       <ul class="flex flex-row items-center sm:mt-4 sm:pt-4 md:mt-0 md:pt-0 md:mr-4 lg:mr-8">
         <li>
-          <nuxt-link to="/test" class="block font-medium px-4 py-1 md:p-2 lg:px-4">
+          <nuxt-link to="/test" class="block text-white font-medium px-4 py-1 md:p-2 lg:px-4">
             Test
           </nuxt-link>
         </li>
